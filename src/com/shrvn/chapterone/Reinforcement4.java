88401a3bd23af7c22be6ad6e18c2b097f7d0a58e
shra012 <shravan_fisher@yahoo.ca>
2017-08-14 23:56:45 +0530

    Added fourth sum solution

---
+++ src/com/shrvn/chapterone/Reinforcement4.java
@@ -0,0 +1,20 @@
+package com.shrvn.chapterone;
+
+import java.util.Scanner;
+
+public class Reinforcement4 {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of i");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        if (isEven(i)) {
+            System.out.println("The number is even");
+            return;
+        }
+        System.out.println("The number is odd");
+    }
+
+    public static boolean isEven(int i) {
+        return (i & 1) == 0;
+    }
+}
