cb1b78db9dda91bdfd54d9c435a9232c2bfe9b0b
shra012 <shravan_fisher@yahoo.ca>
2017-08-14 23:11:10 +0530

    Added third sum solution

---
+++ src/com/shrvn/chapterone/Reinforcement3.java
@@ -0,0 +1,30 @@
+package com.shrvn.chapterone;
+
+import java.util.Scanner;
+
+public class Reinforcement3 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Enter the value of n");
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println("Enter the value of m");
+        long m = sc.nextLong();
+        System.out.print("Calculating");
+        Thread.sleep(1000);
+        System.out.print(".");
+        Thread.sleep(1000);
+        System.out.print(".");
+        Thread.sleep(1000);
+        System.out.println(".");
+        Thread.sleep(1000);
+        if (isMultiple(n, m)) {
+            System.out.println("Yes n is a multiple of m");
+            return;
+        }
+        System.out.println("No n is a not multiple of m");
+    }
+
+    public static boolean isMultiple(long n, long m) {
+        return n % m == 0;
+    }
+}
